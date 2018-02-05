e75ceda34946e952e04843dc60d1b46524840f32
MenyScarlet <melania.gallego.22@gmail.com>
2018-02-05 20:37:43 +0100

    validaciones

--- app/src/main/java/com/example/didact/u3_edittext_ejerregistro/MainActivity.java
+++ app/src/main/java/com/example/didact/u3_edittext_ejerregistro/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.didact.u3_edittext_ejerregistro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup rgSexo;
     Spinner spProvincia;
     CheckBox cbEstudiante;
+    TextView tvResultado;
 
 
 
@@ -30,24 +34,86 @@ public class MainActivity extends AppCompatActivity {
         rgSexo=(RadioGroup)findViewById(R.id.rgSexo);
         spProvincia=(Spinner) findViewById(R.id.spProvincia);
         cbEstudiante=(CheckBox) findViewById(R.id.cbEstudiante);
+        tvResultado=(TextView) findViewById(R.id.tvResultado);
+
+        //Contenido Spinner
+        String [] provincias = {"Selecciona","Cadiz","Sevilla","Huelva","Málaga","Córdoba","Granada",
+                "Jaén","Almería"};
+        ArrayAdapter<String> adaptador= new ArrayAdapter<String>(this,
+                android.R.layout.simple_expandable_list_item_1, provincias);
+        spProvincia.setAdapter(adaptador);
 
 
     }//FIN onCreate
 
+    //Boton
     public void clickVer (View view){
-
+        //Declarar EditTet
         String nombre = etNombre.getText().toString();
         String apellidos = etApellidos.getText().toString();
         String edad = etEdad.getText().toString();
 
+        //Declarar Radio Group
         int id = rgSexo.getCheckedRadioButtonId();
         RadioButton rbSeleccionado = (RadioButton)findViewById(id);
-        String textoRBSeleccionado= rbSeleccionado.getText().toString();
+        String sexo = ( rbSeleccionado == null ? "" : rbSeleccionado.getText().toString() );
 
-        String provinciaSeleccionada
+        /* String sexo = "";
+        int id = rgSexo.getCheckedRadioButtonId();
+        if ( id != -1 ) {
+            RadioButton rbSeleccionado = (RadioButton)findViewById(id);
+            sexo = rbSeleccionado.getText().toString();
+        } */
+
+        // Código Try Catch //
+        /*int id = rgSexo.getCheckedRadioButtonId();
+        RadioButton rbSeleccionado = (RadioButton)findViewById(id);
+        try{
+            String textoRBSeleccionado = rbSeleccionado.getText().toString();
+        } catch ( Exception e ) {
+            String textoRBSeleccionado = "";
+        }*/
+        // ---------------- //
+
+
+        //Declarar spinner
+        String provinciaSeleccionada= spProvincia.getSelectedItem().toString();
+
+
+        //Declarar CheckBox
+
+        boolean estaChequeado = false; //Por defecto no hay nada chequeado
+        String estudiante = "";
+
+        if (cbEstudiante.isChecked()){
+            estudiante+=cbEstudiante.getText().toString();
+            estaChequeado=true;
+        }
+
+        if (
+             nombre.equals("") ||
+             apellidos.equals("") ||
+             sexo.equals("") ||
+             provinciaSeleccionada.equals("Selecciona")
+            ){ // No se cumple algunas de las validaciones
+
+            Toast.makeText(getApplicationContext(),"Debes indicar los campos obligatorios *",
+                    Toast.LENGTH_LONG).show();
+
+        } else { // Pasa todas las validaciones
+                 tvResultado.setText(
+                                 "Nombre: " + nombre +
+                                 "\nApellidos: " + apellidos +
+                                 "\nEdad: " + edad +
+                                 "\nProvincia: " + provinciaSeleccionada +
+                                 "\nEstudiante: " + ( cbEstudiante.isChecked() ? "Si" : "No" )
+                 );
+        }
 
 
 
 
-    }
+
+
+    }//FIN del Boton
 }//FIN MainActivity
